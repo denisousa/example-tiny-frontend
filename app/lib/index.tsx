a9b422c0d89d7770c96e6fdf7343770eb503519c
Denis Menezes de Sousa <denis.sousa@aluno.uece.br>
2023-03-27 11:34:07 -0300

    in progress

--- app/lib/index.tsx
+++ app/lib/index.tsx
@@ -14,37 +14,7 @@ const ExampleTinyFrontend: React.FC<ExampleTinyFrontendProps> = ({
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Hello {name}! 🎉</h1>
-
-      <div className={styles.descriptionContainer}>
-        <p>
-          I&apos;m a{" "}
-          <a
-            href="https://tiny-frontend.github.io"
-            target="_blank"
-            rel="noreferrer"
-          >
-            tiny frontend
-          </a>{" "}
-          🐰 , I was deployed from{" "}
-          <a
-            href="https://github.com/tiny-frontend/example-tiny-frontend"
-            target="_blank"
-            rel="noreferrer"
-          >
-            this git repository
-          </a>
-          . I&apos;m just a regular React component, but my implementation was
-          loaded at runtime!
-        </p>
-        <p>
-          You pressed my button <strong>{value} times</strong>!
-        </p>
-      </div>
-
-      <Button onClick={() => setValue((value) => value + 1)}>
-        Press me Remix Brazil!
-      </Button>
+      <h1>HELLO NEW MF</h1>
     </div>
   );
 };
